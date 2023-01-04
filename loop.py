2d5e77e46b3f5e193eb932c1d9c01a3b839f9fe7
KevinDonnot <kevin@e-k.fr>
2023-01-04 12:42:35 +0100

    Update loop.py

--- loop.py
+++ loop.py
@@ -7,6 +7,7 @@ import yaml
 import torch
 import kornia
 import torchvision
+import time
 
 import numpy                as np
 import nvdiffrast.torch     as dr
@@ -357,7 +358,7 @@ def loop(cfg):
                 )
                 
                 # out_path = os.path.join( cfg["path"], "meshes_export", "mesh_%d" % COMPTEUR_MESHES )
-                out_path = os.path.join( "/content/gdrive/MyDrive/DESIGN_AI/test/", "meshes_export", "mesh_%d" % COMPTEUR_MESHES )
+                out_path = os.path.join( "/content/gdrive/MyDrive/DESIGN_AI/%d/" % time.time(), "meshes_export", "mesh_%d" % COMPTEUR_MESHES )
                 os.makedirs(out_path)
 
                 obj.write_obj(
